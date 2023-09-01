a84d435a483c00a841ddc0a2fe1fd455d3c822b8
JeRyWu <1365840492@qq.com>
2023-09-01 21:18:01 +0800

    add pcap dealer

--- Scapy Project/sniffer.py
+++ Scapy Project/sniffer.py
@@ -1,5 +1,5 @@
 '''
-Sniff various protocols by scapy frame
+Sniff various protocols like HTTP by scapy frame
 '''
 from scapy.all import sniff
 
@@ -9,9 +9,9 @@ def packet_callback(packet) -> None:
         #if "user" in str(packet['TCP'].payload).lower() or "pass" in str(packet["TCP"].payload).lower():
         print(f"[*] Destination: {packet['IP'].dst}")
         print(f"[*] {str(packet['IP'].payload)}")
-#BPF Grammar
-#  descriptor       direction       protocol
-#host/net/port       src/dst     ip/ip6/tcp/udp
+#! BPF Grammar
+#!   descriptor       direction       protocol
+#! host/net/port       src/dst     ip/ip6/tcp/udp
 
 def main():
     '''entrance'''
